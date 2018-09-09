d55f20e5165849b6d46f41de95254aea408b33a3
Sobhit Kumar <sobhit2010@gmail.com>
2018-09-09 07:24:23 +0530

    add file conditioning

--- simplescript.sh
+++ simplescript.sh
@@ -46,15 +46,6 @@ else
   echo "you are not admin"
 fi
 
-# COMPARISON
-# NUM1=31
-# NUM2=5
-# if [ "$NUM1" -gt "$NUM2" ]
-# then
-#   echo "$NUM1 is greater than $NUM2"
-# else
-#   echo "$NUM1 is less than $NUM2"
-# fi
 
 NUM1=10
 NUM2=7
@@ -64,7 +55,6 @@ then
 else
   echo "$NUM1 is less than $NUM2"
 fi
-
 ########
 # val1 -eq val2 Returns true if the values are equal
 # val1 -ne val2 Returns true if the values are not equal
@@ -73,5 +63,29 @@ fi
 # val1 -lt val2 Returns true if val1 is less than val2
 # val1 -le val2 Returns true if val1 is less than or equal to val2
 ########
+
+
+# File Conditions
+FILE="test"
+if [ -f "$FILE" ]
+then 
+  echo "$FILE is a file."
+else  
+  echo "$FILE is not a file."
+fi
+########
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+########
+
+
+
 
 
